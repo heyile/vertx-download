e513b265120fa8528189828f7528d0618048972f
heyile <2513931335@qq.com>
2018-11-22 10:23:11 +0800

    init

---
+++ src/main/java/com/rkd/HttpClientTest.java
@@ -0,0 +1,41 @@
+package com.rkd;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.file.AsyncFile;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.core.http.*;
+import io.vertx.core.streams.Pump;
+
+public class HttpClientTest {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        String filePath = "E:\\vertx\\vertx-download\\src\\main\\java\\com\\rkd\\to.txt";
+        HttpClientOptions http2ClientOption = new HttpClientOptions();
+        http2ClientOption.setProtocolVersion(HttpVersion.HTTP_2)
+                .setUseAlpn(true)
+                .setHttp2ClearTextUpgrade(false)
+                .setTrustAll(true)
+                .setSsl(true)
+                .setDefaultPort(8080)
+                .setDefaultHost("localhost");
+        HttpClient httpClient = vertx.createHttpClient(http2ClientOption);
+        HttpClientRequest httpClientRequest = httpClient.request(HttpMethod.POST, "", res -> {
+            res.pause();
+            vertx.fileSystem().open(filePath, new OpenOptions(), ares -> {
+                AsyncFile file = ares.result();
+                Pump pump = Pump.pump(res, file);
+                res.endHandler(v1 -> {
+                    System.out.println("end...");
+                    file.flush().close(v2 -> {
+                        System.out.println(" download success");
+                    });
+                });
+                pump.start();
+                res.resume();
+            });
+        });
+        httpClientRequest.setChunked(true);
+        httpClientRequest.end("hello");
+    }
+
+}
---
+++ src/main/java/com/rkd/HttpServerDownloadTest.java
@@ -0,0 +1,32 @@
+package com.rkd;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.PemKeyCertOptions;
+
+public class HttpServerDownloadTest {
+    public static void main(String[] args) {
+        HttpServerOptions httpServerOptions = new HttpServerOptions();
+        httpServerOptions.setUseAlpn(true)
+                .setSsl(true)
+                .setPemKeyCertOptions(new PemKeyCertOptions().setKeyPath("tsl/server-key.pem").setCertPath("tsl/server-cert.pem"));
+        Vertx vertx = Vertx.vertx();
+        HttpServer httpServer = vertx.createHttpServer(httpServerOptions);
+        httpServer.requestHandler(request -> {
+            request.bodyHandler(body -> {
+                System.out.println("server : " + body.toString());
+            });
+            String file = "E:\\vertx\\vertx-download\\src\\main\\java\\com\\rkd\\from.txt";
+            request.response().headers().set("Content-Disposition", "attachment;filename=tempFileEntity.txt");
+            request.response().sendFile(file);
+        });
+        httpServer.listen(8080, "localhost", req -> {
+            if (req.succeeded()) {
+                System.out.println("bind port :" + req.result().actualPort());
+            } else {
+                System.out.println("bind port error");
+            }
+        });
+    }
+}
